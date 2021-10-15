e90ce2fce3b99624f3eec2ab03f257b66b83cfc9
Will <floppadonk@gmail.com>
2021-10-15 17:07:12 +1100

    Added Event Form to forms.py

--- projectfile/website/forms.py
+++ projectfile/website/forms.py
@@ -1,7 +1,23 @@
-
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
+from wtforms.fields.core import IntegerField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+allowed_file = {'png', 'jpg', 'PNG', 'JPG'}
+
+#Event form
+class EventForm(FlaskForm):
+    #Event name
+    name = StringField('Venue', validators=[InputRequired()])
+    #Event description
+    description = TextAreaField('Description', validators=[InputRequired()])
+    #Event Image
+    image = FileField('Event Image', validators=[FileRequired(message='Image must be uploaded'), 
+    FileAllowed(allowed_file, message='Sorry, only .png and .jpg files allowed')])
+    #Event price
+    price = IntegerField('Price', validators=[InputRequired()])
+    submit = SubmitField('Create event')
 
 
 #creates the login information
